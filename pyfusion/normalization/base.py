a14059274da4eac25e3f9828ae3df07cb147c8dd
Hervé BREDIN <bredin@limsi.fr>
2012-07-05 21:48:14 +0200

    ZNorm & Sigmoid normalization

---
+++ pyfusion/normalization/base.py
@@ -0,0 +1,57 @@
+import numpy as np
+import scipy.stats
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class ZNorm(BaseEstimator, TransformerMixin):
+    """
+    Z-normalization
+    
+    Parameters
+    ----------
+    copy : boolean, optional
+        If False, .transform() is applied inplace.
+    """
+    
+    def __init__(self, copy=True):
+        super(ZNorm, self).__init__()
+        self.copy = copy
+    
+    def fit(self, X, y=None):
+        
+        # make X.shape == (n, d)
+        X = np.atleast_2d(X)
+        N, D = X.shape
+        
+        self.mean_ = np.mean(X, axis=0)
+        self.std_ = np.std(X, axis=0)
+        
+        return self
+    
+    def transform(self, X):
+        
+        X = np.atleast_2d(X)
+        nX, D = X.shape
+        if D != len(self.mean_):
+            raise ValueError('X shape mismatch')
+        
+        if self.copy:
+            X = X.copy()
+        
+        return (X - self.mean_) / self.std_
+
+
+class Sigmoid(ZNorm):
+    """
+    Sigmoid normalization
+    
+    Parameters
+    ----------
+    copy : boolean, optional
+        If False, .transform() is applied inplace.
+    """
+    def __init__(self, copy=True):
+        super(Sigmoid, self).__init__()
+        self.copy = copy
+        
+    def transform(self, X):
+        return .5*(1+np.tanh(.01*super(Sigmoid, self).transform(X)))
